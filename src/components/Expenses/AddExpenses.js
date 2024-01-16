19e67823dd9191d845537c418b1263585c46a7f4
Noor Ayesha <ayeshashaikh0222@gmail.com>
2024-01-16 15:04:48 +0530

    Integrating it with firebase

--- src/components/Expenses/AddExpenses.js
+++ src/components/Expenses/AddExpenses.js
@@ -1,18 +1,27 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useContext, useEffect} from "react";
 import {Form, Container, Button} from "react-bootstrap";
 import styleSheet from "./AddExpenses.module.css";
+import AuthContext from "../../Store/AuthContext";
 
 
 const AddExpenses = ({onAddExpense}) => {
 
+    const authcontext = useContext(AuthContext);
+
+    const currencyInputRef = useRef();
     const categoryInputRef = useRef();
     const descriptionInputRef = useRef();
     const amountInputRef= useRef();
 
+    const [currency, setCurrency] = useState("");
     const [category, setCategory] = useState("");
     const [description, setDescription] = useState("");
     const [amount, setAmount] = useState("");
 
+
+   const currencyInputChangeHandler = () => {
+     setCurrency(currencyInputRef.current.value);
+   };
 
    const catergoryInputChangeHandler = () => {
     setCategory(categoryInputRef.current.value);
@@ -26,38 +35,80 @@ const AddExpenses = ({onAddExpense}) => {
     setAmount(amountInputRef.current.value);
    };
 
-   const addExpenseSubmitHandler = (event) => {
+   const submitHandler = (event) => {
         event.preventDefault();
 
         const expenseData = {
+            currency: currency,
             category: category,
             description: description,
             amount: amount
         }
-        onAddExpense(expenseData);
+        addExpenseHandler(expenseData);
         setCategory("");
         setDescription("");
         setAmount("");
         
    };
 
+   const email = authcontext.email.replace(/[^a-zA-Z0-9]/g, "");
+
+    useEffect = (() => {
+        fetch(`https://expensedata-34e5e-default-rtdb.firebaseio.com/expenses${email}.json`, {
+        method: "POST",
+        body: JSON.stringify([])
+      })
+    }, [email])
+
+    const addExpenseHandler = (expenseData) => {
+       fetch(`https://expensedata-34e5e-default-rtdb.firebaseio.com/expenses${email}.json`, {
+        method: "POST",
+        body: JSON.stringify(expenseData),
+        headers: {
+            "content-Type": "application/json",
+        },
+       }).then((res)=> {
+        if(res.ok){
+            console.log("successful");
+            return res.json()
+        } else {
+            return res.json().then((data)=> {
+                alert("Something went wrong");
+            })
+        }
+       
+       })
+    };
+
     return (
         <>
         <Container breakpoints={["xxxl","xxl", "xl", "lg", "md", "sm", "xs", "xxs"]} minbreakpoint="xxs"
         className={styleSheet["add-expense"]}>
             <h5 className={styleSheet.title}>Add New Expense</h5>
-            <Form onSubmit={addExpenseSubmitHandler}>
+            <Form onSubmit={submitHandler}>
+              <Form.Group className={styleSheet["form-group"]} style={{display:'flex',flexDirection:"row",alignItems:"center"}}>
+              <Form.Label>Amount: </Form.Label>
+                 <Form.Select className={styleSheet["form-controls"]} aria-label="expensecurrency"
+                  ref={currencyInputRef} 
+                  value={currency} 
+                  onChange={currencyInputChangeHandler}>
+                  <option value={null}>Select Currency</option>
+                  <option value="$">$</option>
+                  <option value="₹">₹</option>
+                  <option value="€">€</option>
+                 </Form.Select>
+              </Form.Group>  
               <Form.Group className={styleSheet["form-group"]}>
                 <Form.Select  aria-label="expenseCategroy"
                ref = {categoryInputRef}
                value={category}
                onChange ={catergoryInputChangeHandler}
               className={styleSheet["form-controls"]}>
-                <option>Select Where you spend</option>
-                <option>Car servicing</option>
-                <option>petrol</option>
-                <option>Food</option>
-                <option>Grocery</option>
+                <option value={null}>Select Where you spend</option>
+                <option value="car servicing">Car servicing</option>
+                <option value="petrol">petrol</option>
+                <option value="food">Food</option>
+                <option value="grocery">Grocery</option>
                 </Form.Select>
               </Form.Group> 
               <Form.Group className={styleSheet["form-group"]}>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, {useState, useEffect, useContext} from "react";
 import { Container, ListGroup } from "react-bootstrap";
 import styleSheet from "./Expenses.module.css";
+import AuthContext from "../../Store/AuthContext";
 
-const Expenses = ({ expenses }) => {
+const Expenses = () => {
+
+    const authcontext = useContext(AuthContext);
+
+    const [expenses, setExpenses] = useState([]);
+
+    const email = authcontext.email.replace(/[^a-zA-Z0-9]/g, "");
+
+    const fetchExpenseHandler = () => {
+        fetch(`https://expensedata-34e5e-default-rtdb.firebaseio.com/expenses${email}.json`).then((res) => {
+            if(res.ok){
+                console.log("Successful");
+                return res.json();
+            } else {
+                return res.json().then((data)=> {
+                    console.log("Failed to fetch expenses");
+                });
+            }
+        }).then((data) => {
+            let fetchedExpenses = []
+            for(const key in data){
+                fetchedExpenses.push({
+                    id: key,
+                    currency: data[key].currency,
+                    category: data[key].category,
+                    description: data[key].description,
+                    amount: data[key].amount
+                })
+            }
+            setExpenses(fetchedExpenses);
+        }).catch((error) => {
+            console.log("Error", error);
+        })
+    };
+
+    useEffect(() => {
+        fetchExpenseHandler();
+    }, [fetchExpenseHandler]);
+
 
     return (
         <>
@@ -12,7 +51,7 @@ const Expenses = ({ expenses }) => {
             <ListGroup as="ul" className={styleSheet.ul}>
                 {expenses.map((expense, index) => (
                    <ListGroup.Item key={index} as="li" style={{color: "#000"}} className={styleSheet.list}>
-                   {expense.category} - {expense.description} -  ${expense.amount}  
+                   {expenses.currency} - {expense.category} - {expense.description} -  ${expense.amount}  
                     
                    </ListGroup.Item>
                      
